4bb4d588f1ab9933569943b4ed0a59ce41156c4d
anilyadavrtu <anilyadav.rtu@gmail.com>
2020-01-16 21:33:08 +0530

    base project setup

---
+++ src/test/java/com/bigstore/bigstorediscoveryservice/BigstoreDiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bigstore.bigstorediscoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BigstoreDiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
