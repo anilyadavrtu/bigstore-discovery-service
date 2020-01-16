b3d939bcd1287c255597aa9cd07b5bb729aff7a6
anilyadavrtu <anilyadav.rtu@gmail.com>
2020-01-16 22:12:02 +0530

    base project setup

--- src/main/java/com/bigstore/bigstorediscoveryservice/BigstoreDiscoveryServiceApplication.java
+++ src/main/java/com/bigstore/bigstorediscoveryservice/BigstoreDiscoveryServiceApplication.java
@@ -2,12 +2,14 @@ package com.bigstore.bigstorediscoveryservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class BigstoreDiscoveryServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BigstoreDiscoveryServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BigstoreDiscoveryServiceApplication.class, args);
+    }
 
 }
